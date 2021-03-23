2ab579c20869ffd76a75c653037f934f5e5159b8
TANMAY DOKE <doketanmay01@gmail.com>
2021-03-23 17:18:38 +0530

    Changes on existing line

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("welcome");
-		System.out.println("Line from new folder");
+		System.out.println("Line from new folder and some changes");
 	}
 }
