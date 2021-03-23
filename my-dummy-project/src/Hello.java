8ace9035b3a408f039454df019971ca69a1892cf
TANMAY DOKE <doketanmay01@gmail.com>
2021-03-23 17:13:25 +0530

    from new eclipse file

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("welcome");
+		System.out.println("Line from new folder");
 	}
 }
