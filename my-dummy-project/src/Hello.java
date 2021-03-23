932294d4624cc0f9fcc878d789a94c8f10e8c3fc
TANMAY DOKE <doketanmay01@gmail.com>
2021-03-23 17:21:10 +0530

    Changes from file 2

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("welcome");
-		System.out.println("Line from new folder and some changes");
+		System.out.println("Line from new folder and some changes...");
 	}
 }
