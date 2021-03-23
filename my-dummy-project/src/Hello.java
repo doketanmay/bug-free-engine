02b852503d59fa4a845ca77d6a63b0c89a0149d5
TANMAY DOKE <doketanmay01@gmail.com>
2021-03-23 17:17:08 +0530

    Small changes on line 3

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("welcome");
-		System.out.println("Line from new folder");
+		System.out.println(" Updated Line from new folder");
 	}
 }
